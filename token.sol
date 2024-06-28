5e728094f3148faa571c899c9d45bc7de6f14f03
Utkarsh Kumar Singh <120135884+JAI-REX@users.noreply.github.com>
2024-06-28 11:02:16 +0530

    Create token.sol

---
+++ token.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.18;
+
+contract Mytoken{
+    string public Mytoken_Name = "TREX";
+    string public Mytoken_abbrv = "REX";
+    uint public total_Supply = 0;
+
+    mapping(address => uint)public balance;
+
+    function mint(address Add , uint Val) public{
+        total_Supply += Val;
+        balance[Add] += Val;
+
+    }
+
+    function burning(address Add, uint Val) public{
+        if(balance[Add] >= Val){
+            total_Supply -= Val;
+            balance[Add] -= Val;
+        }
+    }
+}
